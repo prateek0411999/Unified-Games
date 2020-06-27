34b3c1cd2e724ee0b6089ec3ca95959145fe9a81
Prateek Sharma <prateekshrmprateek@gmail.com>
2020-06-27 21:29:55 +0530

    on-going
    added validations

---
+++ unifiedGames/unifiedGames/shared/first-name.validator.ts
@@ -0,0 +1,9 @@
+import { AbstractControl } from "@angular/forms";
+
+export function forbiddenNameValidator(control: AbstractControl): {[key: string]: any} | null {
+
+    const forbidden= /admin/.test(control.value);
+    
+    return forbidden ? { 'forbiddenName' : {value: control.value} }: null;
+
+}
--- unifiedGames/unifiedGames/src/app/signup/signup.component.ts
+++ unifiedGames/unifiedGames/src/app/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
+import { forbiddenNameValidator } from 'shared/first-name.validator';
+import { PasswordValidator } from 'shared/password.validator';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -16,13 +18,14 @@ export class SignupComponent implements OnInit {
   //form group instance that will represent the reactive form
 
   signUpForm = this.fb.group({
-    firstname: ['Prateek'],
-    lastname: [''],
-    username: [''],
-    email: [''],
+    firstname: ['',[Validators.required,Validators.minLength(3),forbiddenNameValidator]],
+    lastname: ['',[Validators.required,Validators.minLength(3),forbiddenNameValidator]],
+    
+    email: ['',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
+    contactno: ['',[Validators.required,Validators.pattern("^[7-9][0-9]{9}$")]],
     password: [''],
     confirmPassword: ['']
-  });
+  },{validators: PasswordValidator});
 
 
 }
